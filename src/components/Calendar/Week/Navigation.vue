e63f6dffad6cc8c0ff5b418e0921f63060e0e9eb
Khaled <61189819+Khaled-Aller@users.noreply.github.com>
2023-02-15 18:12:28 +0100

    Small fixes

--- src/components/Calendar/Week/Navigation.vue
+++ src/components/Calendar/Week/Navigation.vue
@@ -3,7 +3,7 @@
     <h2 class="font-medium hover-transition hover:text-white hover:bg-primary rounded-md cursor-pointer py-1 px-2" :class="weekNumberNavigation ? 'bg-primary text-white' : 'text-gray-600'" @click="weekNumberNavigation = !weekNumberNavigation">Uge {{ weekNumber }} - {{ year }}</h2>
     <div v-if="weekNumberNavigation" class="absolute w-[300px] mt-5 hover-transition z-[200]" v-click-outside="hideWeekNumberNavigation">
       <div class="w-0 h-0 border-[15px] absolute top-0 left-[20px] mt-[-30px] border-l-transparent border-r-transparent border-t-transparent border-b-gray-300"></div>
-      <DatePicker is-expanded show-iso-weeknumbers is_range :attributes="highlightDatesConfig" v-model="date" @dayclick="onDayClick"/>
+      <DatePicker is-expanded show-iso-weeknumbers :attributes="highlightDatesConfig" v-model="date" @dayclick="onDayClick"/>
     </div>
   </div>
 </template>
